325fa4dceafb9c80994b0da7e403865da83ba5fa
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-09-29 19:01:31 +0100

    Final changes added!

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import React from "react";
 
-// Preloader
-import { Circle2 } from "react-preloaders";
-
 // Sections
 import { Header, Home, About, Projects, Skills, Contact } from "./sections";
 
 const App = () => {
   return (
     <>
-      <Circle2
-        time={1000}
-        background="#0a192f"
-        color="#68ACCD"
-        animation="fade"
-      />
-
       <Header />
       <Home />
       <About />
