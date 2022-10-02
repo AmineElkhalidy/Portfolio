e021785f66d2e372169504df9d10fbb3e7f2f199
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-10-02 14:49:28 +0100

    Adding a new project to the projects section

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -8,6 +8,7 @@ import { FiGithub } from "react-icons/fi";
 import { motion } from "framer-motion";
 
 // Data
+import Mamiru from "../assets/images/projects/mamiru.png";
 import Omnifood from "../assets/images/projects/project-01.png";
 import LandingPage from "../assets/images/projects/project-02.png";
 import ShopStore from "../assets/images/projects/project-03.png";
@@ -35,6 +36,56 @@ const Projects = () => {
 
         {/* Work Container */}
         <motion.div className="flex flex-col space-y-28">
+          {/* Mamiru */}
+          <motion.div
+            whileInView={{ opacity: [0, 1] }}
+            className="relative grid bg-[#112240] rounded-md shadow-xl p-4 md:bg-transparent md:rounded-none md:shadow-none md:p-0 md:grid-cols-2 md:px-16"
+          >
+            {/* Project Image */}
+            <div className="relative w-full h-full rounded-md overflow-hidden">
+              <div className="absolute top-0 left-0 w-full h-full bg-slate-800/50 duration-300 hover:cursor-pointer hover:bg-transparent"></div>
+              <img className="h-full" src={Mamiru} alt="Project preview" />
+            </div>
+
+            {/* Project Description */}
+            <div className="mt-6 text-center md:text-right">
+              <h4 className="font-nav font-medium text-secondary tracking-wider">
+                Featured Project
+              </h4>
+
+              <h3 className="text-lg font-semibold mb-4 lg:text-2xl">Mamiru</h3>
+
+              <p className="max-w-md mx-auto text-slate-200 text-left my-6 md:text-right md:p-4 md:bg-[#112240] md:shadow-xl md:rounded-md md:mr-0">
+                Mamiru is an e-commerce website under construction built using
+                react and redux with Typescript.
+              </p>
+
+              <ul className="max-w-md mx-auto flex flex-wrap justify-start items-center font-nav text-slate-300 md:justify-end gap-6 mb-6 mr-0">
+                <li className="">React</li>
+                <li className="">Redux</li>
+                <li className="">TypeScript</li>
+              </ul>
+
+              <div className="max-w-md mx-auto text-slate-200 flex justify-start items-center md:justify-end gap-6 mr-0 md:mb-4">
+                <a
+                  href="https://github.com/SamoudiAnas/Mamirou"
+                  target="_blank"
+                  className="hover:text-secondary"
+                >
+                  <FiGithub size={23} className="cursor-pointer" />
+                </a>
+
+                <a
+                  href="https://mamirou.vercel.app/"
+                  target="_blank"
+                  className="hover:text-secondary"
+                >
+                  <HiOutlineExternalLink size={24} className="cursor-pointer" />
+                </a>
+              </div>
+            </div>
+          </motion.div>
+
           {/* Omnifood */}
           <motion.div
             whileInView={{ opacity: [0, 1] }}
