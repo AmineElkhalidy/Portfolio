2062eab95ede794ff284a2c5a03965741efcdc7f
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-10-03 21:00:12 +0100

    Adding an other featured project

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -9,6 +9,7 @@ import { motion } from "framer-motion";
 
 // Data
 import Mamiru from "../assets/images/projects/mamiru.png";
+import Tasty from "../assets/images/projects/tasty.png";
 import Omnifood from "../assets/images/projects/project-01.png";
 import LandingPage from "../assets/images/projects/project-02.png";
 import ShopStore from "../assets/images/projects/project-03.png";
@@ -83,6 +84,55 @@ const Projects = () => {
                   <HiOutlineExternalLink size={24} className="cursor-pointer" />
                 </a>
               </div>
+            </div>
+          </motion.div>
+
+          {/* Tasty */}
+          <motion.div
+            whileInView={{ opacity: [0, 1] }}
+            className="relative grid bg-[#112240] rounded-md shadow-xl p-4 md:bg-transparent md:rounded-none md:shadow-none md:p-0 md:grid-cols-2 md:px-16"
+          >
+            {/* Project Description */}
+            <div className="mb-6 text-center md:text-left">
+              <h4 className="font-nav font-medium text-secondary tracking-wider">
+                Featured Project
+              </h4>
+
+              <h3 className="text-lg font-semibold mb-4 lg:text-2xl">Tasty</h3>
+
+              <p className="max-w-md mx-auto text-slate-200 text-left my-6 md:p-4 md:bg-[#112240] md:shadow-xl md:rounded-md md:ml-0">
+                Simple landing page of a restaurant
+              </p>
+
+              <ul className="max-w-md mx-auto text-slate-300 flex flex-wrap justify-start items-center font-nav md:justify-start gap-6 mb-6 ml-0">
+                <li className="">HTML</li>
+                <li className="">CSS</li>
+                <li className="">JavaScript</li>
+              </ul>
+
+              <div className="max-w-md mx-auto text-slate-200 flex justify-start items-center md:justify-start gap-6 ml-0 mb-4">
+                <a
+                  href="https://github.com/AmineElkhalidy/Tasty"
+                  target="_blank"
+                  className="hover:text-secondary"
+                >
+                  <FiGithub size={23} className="cursor-pointer" />
+                </a>
+
+                <a
+                  href="https://tasty.vercel.app/"
+                  target="_blank"
+                  className="hover:text-secondary"
+                >
+                  <HiOutlineExternalLink size={23} className="cursor-pointer" />
+                </a>
+              </div>
+            </div>
+
+            {/* Project Image */}
+            <div className="relative w-full h-full rounded-md overflow-hidden">
+              <div className="absolute top-0 left-0 w-full h-full bg-slate-800/50 duration-300 hover:cursor-pointer hover:bg-transparent"></div>
+              <img className="h-full" src={Tasty} alt="Project preview" />
             </div>
           </motion.div>
 
@@ -194,58 +244,6 @@ const Projects = () => {
             </div>
           </motion.div>
 
-          {/* Landing Page */}
-          <motion.div
-            whileInView={{ opacity: [0, 1] }}
-            className="relative grid bg-[#112240] rounded-md shadow-xl p-4 md:bg-transparent md:rounded-none md:shadow-none md:p-0 md:grid-cols-2 md:px-16"
-          >
-            {/* Project Image */}
-            <div className="relative w-full h-full rounded-md overflow-hidden">
-              <div className="absolute top-0 left-0 w-full h-full bg-slate-800/50 duration-300 hover:cursor-pointer hover:bg-transparent"></div>
-              <img className="h-full" src={LandingPage} alt="Project preview" />
-            </div>
-
-            {/* Project Description */}
-            <div className="mt-6 text-center md:text-right">
-              <h4 className="font-nav font-medium text-secondary tracking-wider">
-                Featured Project
-              </h4>
-
-              <h3 className="text-lg font-semibold mb-4 lg:text-2xl">
-                Startup Landing
-              </h3>
-
-              <p className="max-w-md mx-auto text-slate-200 text-left my-6 md:text-right md:p-4 md:bg-[#112240] md:shadow-xl md:rounded-md md:mr-0">
-                Simple startup landing page that was built using Next.js, a
-                React framework for production, with amazing curves, color
-                scheme and animation...
-              </p>
-
-              <ul className="max-w-md mx-auto flex flex-wrap justify-start items-center font-nav text-slate-300 md:justify-end gap-6 mb-6 mr-0">
-                <li className="">React</li>
-                <li className="">Next.js</li>
-              </ul>
-
-              <div className="max-w-md mx-auto text-slate-200 flex justify-start items-center md:justify-end gap-6 mr-0 md:mb-4">
-                <a
-                  href="https://github.com/AmineElkhalidy/Landing-Page"
-                  target="_blank"
-                  className="hover:text-secondary"
-                >
-                  <FiGithub size={23} className="cursor-pointer" />
-                </a>
-
-                <a
-                  href="https://landing-page-startup.vercel.app/"
-                  target="_blank"
-                  className="hover:text-secondary"
-                >
-                  <HiOutlineExternalLink size={24} className="cursor-pointer" />
-                </a>
-              </div>
-            </div>
-          </motion.div>
-
           {!show && (
             <motion.div
               className="inline-block text-center"
@@ -262,6 +260,65 @@ const Projects = () => {
 
           {show && (
             <>
+              {/* Landing Page */}
+              <motion.div
+                whileInView={{ opacity: [0, 1] }}
+                className="relative grid bg-[#112240] rounded-md shadow-xl p-4 md:bg-transparent md:rounded-none md:shadow-none md:p-0 md:grid-cols-2 md:px-16"
+              >
+                {/* Project Image */}
+                <div className="relative w-full h-full rounded-md overflow-hidden">
+                  <div className="absolute top-0 left-0 w-full h-full bg-slate-800/50 duration-300 hover:cursor-pointer hover:bg-transparent"></div>
+                  <img
+                    className="h-full"
+                    src={LandingPage}
+                    alt="Project preview"
+                  />
+                </div>
+
+                {/* Project Description */}
+                <div className="mt-6 text-center md:text-right">
+                  <h4 className="font-nav font-medium text-secondary tracking-wider">
+                    Featured Project
+                  </h4>
+
+                  <h3 className="text-lg font-semibold mb-4 lg:text-2xl">
+                    Startup Landing
+                  </h3>
+
+                  <p className="max-w-md mx-auto text-slate-200 text-left my-6 md:text-right md:p-4 md:bg-[#112240] md:shadow-xl md:rounded-md md:mr-0">
+                    Simple startup landing page that was built using Next.js, a
+                    React framework for production, with amazing curves, color
+                    scheme and animation...
+                  </p>
+
+                  <ul className="max-w-md mx-auto flex flex-wrap justify-start items-center font-nav text-slate-300 md:justify-end gap-6 mb-6 mr-0">
+                    <li className="">React</li>
+                    <li className="">Next.js</li>
+                  </ul>
+
+                  <div className="max-w-md mx-auto text-slate-200 flex justify-start items-center md:justify-end gap-6 mr-0 md:mb-4">
+                    <a
+                      href="https://github.com/AmineElkhalidy/Landing-Page"
+                      target="_blank"
+                      className="hover:text-secondary"
+                    >
+                      <FiGithub size={23} className="cursor-pointer" />
+                    </a>
+
+                    <a
+                      href="https://landing-page-startup.vercel.app/"
+                      target="_blank"
+                      className="hover:text-secondary"
+                    >
+                      <HiOutlineExternalLink
+                        size={24}
+                        className="cursor-pointer"
+                      />
+                    </a>
+                  </div>
+                </div>
+              </motion.div>
+
               {/* Shop Store */}
               <motion.div
                 whileInView={{ opacity: [0, 1] }}
