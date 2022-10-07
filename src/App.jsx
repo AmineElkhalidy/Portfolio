9e5a3b38a1b14e47cf30885fafd7eb36ba0c25ce
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-10-07 19:57:05 +0100

    Google analytics added!

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./App.css";
 
+import ReactGA from "react-ga";
+const TRACKING_ID = "G-49J1KPT5P7"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+
 // Sections
 import { Header, Hero, About, Projects, Skills, Contact } from "./sections";
 
