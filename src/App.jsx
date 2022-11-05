946da9e993bbe655aa4ecd34db9aac3d6f767cee
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-11-05 11:43:10 +0100

    Initializing google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import "./App.css";
 import ReactGA from "react-ga";
 const TRACKING_ID = "G-49J1KPT5P7"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
-
 // Sections
 import { Header, Hero, About, Projects, Skills, Contact } from "./sections";
 
 const App = () => {
+  // Google analytics
+  React.useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <>
       <Header />
