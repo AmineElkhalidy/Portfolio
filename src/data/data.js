e7354ca4f27591efad2d6ef051add36eeb675f0b
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-11-15 15:47:58 +0100

    Data added!

--- src/data/data.js
+++ src/data/data.js
@@ -10,6 +10,78 @@ import Redux from "../assets/images/redux.png";
 import TypeScript from "../assets/images/typescript.png";
 import Git from "../assets/images/git.png";
 
+// Icons
+import { SiUpwork } from "react-icons/si";
+import { BsWhatsapp } from "react-icons/bs";
+
+// Data
+export const rowreviews1 = [
+  {
+    id: "r1",
+    name: "Mossaab Talbi",
+    username: "@Mossaab",
+    description:
+      "Amine is one of the best and skilled developers I have known, he did an amazing job with my project, I will definitely work with him again",
+    platform: SiUpwork,
+  },
+  {
+    id: "r2",
+    name: "Hi Ba",
+    username: "@Hiba",
+    description:
+      "Amine is a great developer who helped me greatly in a very short amount of time and done all the work I required, I would definitely work with him again!",
+    platform: SiUpwork,
+  },
+  {
+    id: "r3",
+    name: "Sacreed Groves",
+    username: "@Grooves",
+    description: "Amine did an amazing job with my react project",
+    platform: BsWhatsapp,
+  },
+  {
+    id: "r4",
+    name: "Aaron Sangerson",
+    username: "@Aaron",
+    description:
+      "Amine made significant changes to an existing react project, integrating additional functionality. I can highly recommend him as a frontend dev",
+    platform: SiUpwork,
+  },
+];
+
+export const rowreviews2 = [
+  {
+    id: "r5",
+    name: "Tireson Emma",
+    username: "@EmmaTey",
+    description: "Amine is a skilled developer that deliver quality work.",
+    platform: BsWhatsapp,
+  },
+  {
+    id: "r6",
+    name: "Rachid Chatoui",
+    username: "@Chatouirachid",
+    description:
+      "Amine helped me a lot with defining the content and layout of my product landing page, definitely recommended!",
+    platform: BsWhatsapp,
+  },
+  {
+    id: "r7",
+    name: "Et-thamry Ashraf",
+    username: "@Et-Achraf",
+    description:
+      "A best friend and best developer, helped me to build my own website and launch it to the internet",
+    platform: BsWhatsapp,
+  },
+  {
+    id: "r8",
+    name: "Anas Samoudi",
+    username: "@Unlimiteddez",
+    description: "Great developer, with sharpen eye on quality check.",
+    platform: BsWhatsapp,
+  },
+];
+
 export const skills = [
   {
     image: Html,
--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 
-// Icons
-import { SiUpwork } from "react-icons/si";
-import { BsWhatsapp } from "react-icons/bs";
-
 // motion
 import { motion } from "framer-motion";
 
@@ -17,72 +13,7 @@ import { AutoScroll } from "@splidejs/splide-extension-auto-scroll";
 import "@splidejs/splide/dist/css/splide.min.css";
 
 // Data
-const rowreviews1 = [
-  {
-    id: "r1",
-    name: "Mossaab Talbi",
-    username: "@Mossaab",
-    description:
-      "Amine is one of the best and skilled developers I have known, he did an amazing job with my project, I will definitely work with him again",
-    platform: SiUpwork,
-  },
-  {
-    id: "r2",
-    name: "Hi Ba",
-    username: "@Hiba",
-    description:
-      "Amine is a great developer who helped me greatly in a very short amount of time and done all the work I required, I would definitely work with him again!",
-    platform: SiUpwork,
-  },
-  {
-    id: "r3",
-    name: "Sacreed Groves",
-    username: "@Grooves",
-    description: "Amine did an amazing job with my react project",
-    platform: BsWhatsapp,
-  },
-  {
-    id: "r4",
-    name: "Aaron Sangerson",
-    username: "@Aaron",
-    description:
-      "Amine made significant changes to an existing react project, integrating additional functionality. I can highly recommend him as a frontend dev",
-    platform: SiUpwork,
-  },
-];
-
-const rowreviews2 = [
-  {
-    id: "r5",
-    name: "Tireson Emma",
-    username: "@EmmaTey",
-    description: "Amine is a skilled developer that deliver quality work.",
-    platform: BsWhatsapp,
-  },
-  {
-    id: "r6",
-    name: "Rachid Chatoui",
-    username: "@Chatouirachid",
-    description:
-      "Amine helped me a lot with defining the content and layout of my product landing page, definitely recommended!",
-    platform: BsWhatsapp,
-  },
-  {
-    id: "r7",
-    name: "Et-thamry Ashraf",
-    username: "@Et-Achraf",
-    description:
-      "A best friend and best developer, helped me to build my own website and launch it to the internet",
-    platform: BsWhatsapp,
-  },
-  {
-    id: "r8",
-    name: "Anas Samoudi",
-    username: "@Unlimiteddez",
-    description: "Great developer, with sharpen eye on quality check.",
-    platform: BsWhatsapp,
-  },
-];
+import { rowreviews1, rowreviews2 } from "../data/data";
 
 const Testimonials = () => {
   return (
@@ -97,10 +28,10 @@ const Testimonials = () => {
           Clients Feedbacks
         </h2>
 
-        <div className="flex flex-col space-y-6">
+        <div className="flex flex-col space-y-4">
           {/* Row 1 */}
           <Splide
-            className="w-full"
+            className="w-full h-full"
             options={{
               type: "loop",
               drag: "free",
@@ -131,7 +62,7 @@ const Testimonials = () => {
 
           {/* Row 2 */}
           <Splide
-            className="w-full"
+            className="w-full h-full"
             options={{
               type: "loop",
               drag: "free",
