b4617a249047fb4b41563f3ad2bccf57c6412e5f
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-12-03 11:55:02 +0100

    Changes added to about section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -25,7 +25,7 @@ const About = () => {
         <div className="max-w-3xl mx-auto md:px-8 lg:px-0">
           <div>
             <p className="text-slate-400 mb-3 md:text-lg">
-              Hi! My name is{" "}
+              Hello 👋, My name is{" "}
               <span className=" text-secondary font-medium">
                 Amine Elkhalidy
               </span>
