0f80e4c825ebdbbd61c95d976afd9c47c287a1b8
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2023-01-20 09:20:27 +0100

    Contact section fixed!

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -36,188 +36,191 @@ const Contact = () => {
     message = "";
   }
   return (
-    <section className="max-w-6xl mx-auto px-6 min-h-screen" id="contact">
-      <motion.div
-        whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
-        transition={{ duration: 0.5 }}
-        className="pt-24"
-      >
-        {/* Container */}
-        <div className="grid gap-16 lg:grid-cols-2 lg:gap-8">
-          {/* Text container */}
-          <div>
-            <h2 className="text-4xl md:text-5xl font-bold text-black-900 mb-8 dark:text-white">
-              Get in touch
-            </h2>
-            <p className="max-w-lg text-grey-400 text-lg leading-7 dark:text-gray-300">
-              Have a project in mind? Looking to partner or work together? Reach
-              out through the form and I'll get back to you in the next 24
-              hours.
-            </p>
+    <section className="h-full w-full" id="contact">
+      <div className="max-w-6xl mx-auto px-6 min-h-screen">
+        <motion.div
+          whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
+          transition={{ duration: 0.5 }}
+          className="pt-24"
+        >
+          {/* Container */}
+          <div className="grid gap-16 md:grid-cols-2 lg:gap-8">
+            {/* Text container */}
+            <div>
+              <h2 className="text-4xl md:text-5xl font-bold text-black-900 mb-8 dark:text-white">
+                Get in touch
+              </h2>
+              <p className="max-w-lg text-grey-400 text-lg leading-7 dark:text-gray-300">
+                Have a project in mind? Looking to partner or work together?
+                Reach out through the form and I'll get back to you in the next
+                24 hours.
+              </p>
 
-            {/* Contact resources container */}
-            <div className="mt-[60px] flex flex-col gap-6">
-              <a
-                href="mailto:a.amineelkhalidy@gmail.com"
-                className="dark:text-gray-100 inline-flex items-center gap-4 text-[20px] font-bold"
-              >
-                <EnvelopeIcon className="w-7 h-7" /> a.amineelkhalidy@gmail.com
-              </a>
-              <a
-                href="tel:+212611154307"
-                className="dark:text-gray-100 inline-flex items-center gap-4 text-[20px] font-bold"
-              >
-                <PhoneIcon className="w-7 h-7" />
-                +212611154307
-              </a>
+              {/* Contact resources container */}
+              <div className="mt-[60px] flex flex-col gap-6">
+                <a
+                  href="mailto:a.amineelkhalidy@gmail.com"
+                  className="dark:text-gray-100 inline-flex items-center gap-4 text-[20px] font-bold"
+                >
+                  <EnvelopeIcon className="w-7 h-7" />{" "}
+                  a.amineelkhalidy@gmail.com
+                </a>
+                <a
+                  href="tel:+212611154307"
+                  className="dark:text-gray-100 inline-flex items-center gap-4 text-[20px] font-bold"
+                >
+                  <PhoneIcon className="w-7 h-7" />
+                  +212611154307
+                </a>
+              </div>
+            </div>
+
+            {/* Form Container */}
+            <div className="mb-16">
+              <form onSubmit={handleSubmit} className="max-w-xl" action="#">
+                {/* First and Last name box */}
+                <div className="flex flex-col mb-6 sm:flex-row sm:gap-6 sm:mb-0">
+                  {/* First name box */}
+                  <div className="mb-6">
+                    <label
+                      className="mb-3 block font-bold text-black-900 dark:text-white"
+                      htmlFor="firstName"
+                    >
+                      First Name
+                    </label>
+                    <input
+                      className="w-full py-3 pl-6 bg-[#f2f6f7] border-none outline-none"
+                      type="text"
+                      name="first-name"
+                      id="firstName"
+                      placeholder="First Name"
+                      value={firstName}
+                      required
+                    />
+                    <ValidationError
+                      prefix="First Name"
+                      field="firstName"
+                      errors={state.errors}
+                    />
+                  </div>
+
+                  {/* Last name box */}
+                  <div className="">
+                    <label
+                      className="mb-3 block font-bold text-black-900 dark:text-white"
+                      htmlFor="lastName"
+                    >
+                      Last Name
+                    </label>
+                    <input
+                      className="w-full py-3 pl-6 bg-[#f2f6f7] border-none outline-none"
+                      type="text"
+                      name="last-name"
+                      id="lastName"
+                      placeholder="Last Name"
+                      value={lastName}
+                      required
+                    />
+                    <ValidationError
+                      prefix="Last Name"
+                      field="lastName"
+                      errors={state.errors}
+                    />
+                  </div>
+                </div>
+
+                {/* Email box */}
+                <div className="mb-6">
+                  <div className="">
+                    <label
+                      className="mb-3 block font-bold text-black-900 dark:text-white"
+                      htmlFor="email"
+                    >
+                      Your E-mail
+                    </label>
+
+                    <input
+                      className="w-full py-3 pl-6 bg-[#f2f6f7] border-none outline-none"
+                      type="text"
+                      name="email"
+                      id="email"
+                      placeholder="Enter your e-mail"
+                      value={email}
+                      required
+                    />
+                    <ValidationError
+                      prefix="Email"
+                      field="email"
+                      errors={state.errors}
+                    />
+                  </div>
+                </div>
+
+                {/* Message box */}
+                <div className="mb-6">
+                  <div className="">
+                    <label
+                      className="mb-3 block font-bold text-black-900 dark:text-white"
+                      htmlFor="message"
+                    >
+                      Tell me a bit more what you are looking for?
+                    </label>
+                    <textarea
+                      className="w-full pt-4 pl-6 outline-none border-none pb-24 bg-[#f2f6f7]"
+                      name="message"
+                      id="message"
+                      maxLength="5000"
+                      placeholder="Explain here"
+                      value={message}
+                      autoCorrect="false"
+                    ></textarea>
+                    <ValidationError
+                      prefix="Message"
+                      field="message"
+                      errors={state.errors}
+                    />
+                  </div>
+                </div>
+
+                {/* Button box */}
+                <div className="relative">
+                  <button
+                    className="inline-block px-[31px] py-5 dark:bg-white dark:text-black-900 bg-black-900 text-white cursor-pointer text-lg duration-300 font-nav font-semibold z-10 hover:translate-x-2 hover:translate-y-2"
+                    href="#contact"
+                    type="submit"
+                    disabled={state.submitting}
+                  >
+                    Submit Now!
+                  </button>
+
+                  <ToastContainer
+                    className="top-20 px-6 sm:px-0"
+                    position="top-center"
+                    autoClose={3000}
+                    hideProgressBar={true}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="colored"
+                  />
+
+                  <span className="contact-btn absolute -z-10 top-2 left-2 px-[5.25rem] py-[2.15rem] bg-red-500" />
+                </div>
+              </form>
             </div>
           </div>
 
-          {/* Form Container */}
-          <div className="mb-32">
-            <form onSubmit={handleSubmit} className="max-w-xl" action="#">
-              {/* First and Last name box */}
-              <div className="flex flex-col mb-6 sm:flex-row sm:gap-6 sm:mb-0">
-                {/* First name box */}
-                <div className="mb-6">
-                  <label
-                    className="mb-3 block font-bold text-black-900 dark:text-white"
-                    htmlFor="firstName"
-                  >
-                    First Name
-                  </label>
-                  <input
-                    className="w-full py-3 pl-6 bg-[#f2f6f7] border-none outline-none"
-                    type="text"
-                    name="first-name"
-                    id="firstName"
-                    placeholder="First Name"
-                    value={firstName}
-                    required
-                  />
-                  <ValidationError
-                    prefix="First Name"
-                    field="firstName"
-                    errors={state.errors}
-                  />
-                </div>
-
-                {/* Last name box */}
-                <div className="">
-                  <label
-                    className="mb-3 block font-bold text-black-900 dark:text-white"
-                    htmlFor="lastName"
-                  >
-                    Last Name
-                  </label>
-                  <input
-                    className="w-full py-3 pl-6 bg-[#f2f6f7] border-none outline-none"
-                    type="text"
-                    name="last-name"
-                    id="lastName"
-                    placeholder="Last Name"
-                    value={lastName}
-                    required
-                  />
-                  <ValidationError
-                    prefix="Last Name"
-                    field="lastName"
-                    errors={state.errors}
-                  />
-                </div>
-              </div>
-
-              {/* Email box */}
-              <div className="mb-6">
-                <div className="">
-                  <label
-                    className="mb-3 block font-bold text-black-900 dark:text-white"
-                    htmlFor="email"
-                  >
-                    Your E-mail
-                  </label>
-
-                  <input
-                    className="w-full py-3 pl-6 bg-[#f2f6f7] border-none outline-none"
-                    type="text"
-                    name="email"
-                    id="email"
-                    placeholder="Enter your e-mail"
-                    value={email}
-                    required
-                  />
-                  <ValidationError
-                    prefix="Email"
-                    field="email"
-                    errors={state.errors}
-                  />
-                </div>
-              </div>
-
-              {/* Message box */}
-              <div className="mb-6">
-                <div className="">
-                  <label
-                    className="mb-3 block font-bold text-black-900 dark:text-white"
-                    htmlFor="message"
-                  >
-                    Tell me a bit more what you are looking for?
-                  </label>
-                  <textarea
-                    className="w-full pt-4 pl-6 outline-none border-none pb-24 bg-[#f2f6f7]"
-                    name="message"
-                    id="message"
-                    maxLength="5000"
-                    placeholder="Explain here"
-                    value={message}
-                    autoCorrect="false"
-                  ></textarea>
-                  <ValidationError
-                    prefix="Message"
-                    field="message"
-                    errors={state.errors}
-                  />
-                </div>
-              </div>
-
-              {/* Button box */}
-              <div className="relative">
-                <button
-                  className="inline-block px-[31px] py-5 dark:bg-white dark:text-black-900 bg-black-900 text-white cursor-pointer text-lg duration-300 font-nav font-semibold z-10 hover:translate-x-2 hover:translate-y-2"
-                  href="#contact"
-                  type="submit"
-                  disabled={state.submitting}
-                >
-                  Submit Now!
-                </button>
-
-                <ToastContainer
-                  className="top-20 px-6 sm:px-0"
-                  position="top-center"
-                  autoClose={3000}
-                  hideProgressBar={true}
-                  newestOnTop={false}
-                  closeOnClick
-                  rtl={false}
-                  pauseOnFocusLoss
-                  draggable
-                  pauseOnHover
-                  theme="colored"
-                />
-
-                <span className="contact-btn absolute -z-10 top-2 left-2 px-[5.25rem] py-[2.15rem] bg-red-500" />
-              </div>
-            </form>
-          </div>
-        </div>
-
-        {/* Copyright text */}
-        <p className="text-black-900 dark:text-white text-sm sm:text-base text-center font-medium">
-          <span>Amine Elkhalidy</span>
-          <br />
-          &copy; {currentYear} - All Rights Reserved
-        </p>
-      </motion.div>
+          {/* Copyright text */}
+          <p className="text-black-900 py-4 dark:text-white text-sm sm:text-base text-center font-medium">
+            <span>Amine Elkhalidy</span>
+            <br />
+            &copy; {currentYear} - All Rights Reserved
+          </p>
+        </motion.div>
+      </div>
     </section>
   );
 };
--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -8,152 +8,151 @@ import { skills } from "../data/data";
 
 const Skills = () => {
   return (
-    <section
-      className="relative w-full h-full mt-24 mb-20 md:mb-40"
-      id="skills"
-    >
-      <motion.div
-        whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
-        transition={{ duration: 0.5 }}
-        className="max-w-6xl mx-auto px-6 lg:max-w-7xl"
-      >
-        <div className="pt-24">
-          {/* Heading */}
-          <h2 className="text-black-900 dark:text-white text-2xl text-center font-nav font-semibold mb-16 sm:text-3xl md:text-4xl lg:text-5xl">
-            Skills & Experience
-          </h2>
+    <section className="w-full h-full" id="skills">
+      <div className="relative mb-10">
+        <motion.div
+          whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
+          transition={{ duration: 0.5 }}
+          className="max-w-6xl mx-auto px-6 lg:max-w-7xl"
+        >
+          <div className="pt-24">
+            {/* Heading */}
+            <h2 className="text-black-900 dark:text-white text-2xl text-center font-nav font-semibold mb-16 sm:text-3xl md:text-4xl lg:text-5xl">
+              Skills & Experience
+            </h2>
 
-          {/* Skills & Experience */}
-          <motion.div className="flex flex-col space-y-20 md:flex-row md:space-y-0">
-            {/* Skills */}
-            <div className="flex flex-wrap gap-10 justify-center items-center md:w-1/2 md:max-w-sm md:mx-auto lg:mb-3">
-              {skills.map((skill) => (
-                <motion.div
-                  key={skill.name}
-                  whileInView={{ opacity: [0, 1] }}
-                  transition={{ duration: 0.5 }}
-                  className="flex flex-col justify-center items-center"
-                >
-                  {/* Image container */}
-                  <div className="bg-gray-100 w-[90px] h-[90px] rounded-full mb-[.75rem] flex justify-center items-center hover:border hover:border-orange-500">
-                    <img
-                      className="w-[60%]"
-                      src={skill.image}
-                      alt={skill.name}
-                    />
-                  </div>
+            {/* Skills & Experience */}
+            <motion.div className="flex flex-col space-y-20 md:flex-row md:space-y-0">
+              {/* Skills */}
+              <div className="flex flex-wrap gap-10 justify-center items-center md:w-1/2 md:max-w-sm md:mx-auto lg:mb-3">
+                {skills.map((skill) => (
+                  <motion.div
+                    key={skill.name}
+                    whileInView={{ opacity: [0, 1] }}
+                    transition={{ duration: 0.5 }}
+                    className="flex flex-col justify-center items-center"
+                  >
+                    {/* Image container */}
+                    <div className="bg-gray-100 w-[90px] h-[90px] rounded-full mb-[.75rem] flex justify-center items-center hover:border hover:border-orange-500">
+                      <img
+                        className="w-[60%]"
+                        src={skill.image}
+                        alt={skill.name}
+                      />
+                    </div>
 
-                  {/* skill name */}
-                  <p className="text-sm font-medium sm:text-md xl:text-lg text-black-400 dark:text-gray-100">
-                    {skill.name}
+                    {/* skill name */}
+                    <p className="text-sm font-medium sm:text-md xl:text-lg text-black-400 dark:text-gray-100">
+                      {skill.name}
+                    </p>
+                  </motion.div>
+                ))}
+              </div>
+
+              {/* Experience */}
+              <div className="flex flex-col space-y-12 md:w-1/2 md:pt-4">
+                {/* 2023 */}
+                <div className="flex space-x-12">
+                  <p className="gradient-text text-md sm:text-lg lg:text-xl xl:text-2xl font-semibold">
+                    2023
                   </p>
-                </motion.div>
-              ))}
-            </div>
 
-            {/* Experience */}
-            <div className="flex flex-col space-y-12 md:w-1/2 md:pt-4">
-              {/* 2023 */}
-              <div className="flex space-x-12">
-                <p className="gradient-text text-md sm:text-lg lg:text-xl xl:text-2xl font-semibold">
-                  2023
-                </p>
+                  <div>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                    >
+                      <h4 className="text-black-900 dark:text-white text-md sm:text-lg lg:text-xl xl:text-2xl font-medium uppercase mb-[.75rem]">
+                        FrontEnd Developer
+                      </h4>
+                      <p className="text-black-400 dark:text-gray-300 mb-[.5rem] xl:text-lg">
+                        I am working as a freelancer on Upwrok
+                      </p>
+                      <p className="text-sm font-nav dark:text-gray-300 text-black-400 xl:text-base">
+                        Upwork
+                      </p>
+                    </motion.div>
+                  </div>
+                </div>
 
-                <div>
-                  <motion.div
-                    whileInView={{ opacity: [0, 1] }}
-                    transition={{ duration: 0.5 }}
-                  >
-                    <h4 className="text-black-900 dark:text-white text-md sm:text-lg lg:text-xl xl:text-2xl font-medium uppercase mb-[.75rem]">
-                      FrontEnd Developer
-                    </h4>
-                    <p className="text-black-400 dark:text-gray-300 mb-[.5rem] xl:text-lg">
-                      I am working as a freelancer on Upwrok
-                    </p>
-                    <p className="text-sm font-nav dark:text-gray-300 text-black-400 xl:text-base">
-                      Upwork
-                    </p>
-                  </motion.div>
+                {/* 2022 */}
+                <div className="flex space-x-12">
+                  <p className="gradient-text text-md sm:text-lg lg:text-xl xl:text-2xl font-semibold ">
+                    2022
+                  </p>
+
+                  <div>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                    >
+                      <h4 className="text-black-900 dark:text-white text-md sm:text-lg lg:text-xl xl:text-2xl font-medium uppercase mb-[.75rem]">
+                        FrontEnd Developer
+                      </h4>
+                      <p className="text-black-400 dark:text-gray-300 mb-[.5rem] xl:text-lg">
+                        I am working as a freelancer
+                      </p>
+                      <p className="text-sm font-nav dark:text-gray-300 text-black-400 xl:text-base">
+                        Freelancer
+                      </p>
+                    </motion.div>
+                  </div>
+                </div>
+
+                {/* 2021 */}
+                <div className="flex space-x-12">
+                  <p className="gradient-text text-md sm:text-lg lg:text-xl xl:text-2xl  font-semibold">
+                    2021
+                  </p>
+
+                  <div>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                      className="ml-1"
+                    >
+                      <h4 className="text-black-900 dark:text-white text-md sm:text-lg lg:text-xl xl:text-2xl font-medium uppercase mb-[.75rem]">
+                        FrontEnd & Alfresco Dev
+                      </h4>
+                      <p className="text-black-400 dark:text-gray-300  mb-[.5rem] xl:text-lg">
+                        I have worked on customising Alfresco (CMS) for a large
+                        organization
+                      </p>
+                      <p className="text-sm font-nav dark:text-gray-300 text-black-400 xl:text-base">
+                        Prefecture d'arrondissement de Ben M'sick
+                      </p>
+                    </motion.div>
+                  </div>
+                </div>
+
+                {/* 2020 */}
+                <div className="flex space-x-12">
+                  <p className="gradient-text text-md sm:text-lg lg:text-xl xl:text-2xl font-semibold">
+                    2020
+                  </p>
+
+                  <div>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                    >
+                      <h4 className="text-black-900 dark:text-white text-md sm:text-lg lg:text-xl xl:text-2xl font-medium uppercase mb-[.75rem]">
+                        FrontEnd Developer
+                      </h4>
+                      <p className="text-grey-400 dark:text-gray-300 mb-[.5rem] xl:text-lg">
+                        I have developped many frontend projects
+                      </p>
+                      <p className="text-sm font-nav dark:text-gray-300 text-grey-400 xl:text-base">
+                        By myslef
+                      </p>
+                    </motion.div>
+                  </div>
                 </div>
               </div>
-
-              {/* 2022 */}
-              <div className="flex space-x-12">
-                <p className="gradient-text text-md sm:text-lg lg:text-xl xl:text-2xl font-semibold ">
-                  2022
-                </p>
-
-                <div>
-                  <motion.div
-                    whileInView={{ opacity: [0, 1] }}
-                    transition={{ duration: 0.5 }}
-                  >
-                    <h4 className="text-black-900 dark:text-white text-md sm:text-lg lg:text-xl xl:text-2xl font-medium uppercase mb-[.75rem]">
-                      FrontEnd Developer
-                    </h4>
-                    <p className="text-black-400 dark:text-gray-300 mb-[.5rem] xl:text-lg">
-                      I am working as a freelancer
-                    </p>
-                    <p className="text-sm font-nav dark:text-gray-300 text-black-400 xl:text-base">
-                      Freelancer
-                    </p>
-                  </motion.div>
-                </div>
-              </div>
-
-              {/* 2021 */}
-              <div className="flex space-x-12">
-                <p className="gradient-text text-md sm:text-lg lg:text-xl xl:text-2xl  font-semibold">
-                  2021
-                </p>
-
-                <div>
-                  <motion.div
-                    whileInView={{ opacity: [0, 1] }}
-                    transition={{ duration: 0.5 }}
-                    className="ml-1"
-                  >
-                    <h4 className="text-black-900 dark:text-white text-md sm:text-lg lg:text-xl xl:text-2xl font-medium uppercase mb-[.75rem]">
-                      FrontEnd & Alfresco Dev
-                    </h4>
-                    <p className="text-black-400 dark:text-gray-300  mb-[.5rem] xl:text-lg">
-                      I have worked on customising Alfresco (CMS) for a large
-                      organization
-                    </p>
-                    <p className="text-sm font-nav dark:text-gray-300 text-black-400 xl:text-base">
-                      Prefecture d'arrondissement de Ben M'sick
-                    </p>
-                  </motion.div>
-                </div>
-              </div>
-
-              {/* 2020 */}
-              <div className="flex space-x-12">
-                <p className="gradient-text text-md sm:text-lg lg:text-xl xl:text-2xl font-semibold">
-                  2020
-                </p>
-
-                <div>
-                  <motion.div
-                    whileInView={{ opacity: [0, 1] }}
-                    transition={{ duration: 0.5 }}
-                  >
-                    <h4 className="text-black-900 dark:text-white text-md sm:text-lg lg:text-xl xl:text-2xl font-medium uppercase mb-[.75rem]">
-                      FrontEnd Developer
-                    </h4>
-                    <p className="text-grey-400 dark:text-gray-300 mb-[.5rem] xl:text-lg">
-                      I have developped many frontend projects
-                    </p>
-                    <p className="text-sm font-nav dark:text-gray-300 text-grey-400 xl:text-base">
-                      By myslef
-                    </p>
-                  </motion.div>
-                </div>
-              </div>
-            </div>
-          </motion.div>
-        </div>
-      </motion.div>
+            </motion.div>
+          </div>
+        </motion.div>
+      </div>
     </section>
   );
 };
