f5f3b1df67ade59704d2b3149663e70278202fca
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2023-01-30 21:47:45 +0100

    Pushing new changes

--- src/sections/Hobbies.jsx
+++ src/sections/Hobbies.jsx
@@ -24,19 +24,19 @@ const Hobbies = () => {
           </h2>
 
           <div className="grid gap-10 sm:grid-cols-2 sm:justify-items-center md:grid-cols-3">
-            <div className="flex items-center justify-center flex-col gap-2 dark:text-gray-200">
+            <div className="flex items-center justify-center flex-col gap-2 dark:text-gray-200 hover:text-orange-400 duration-300">
               <BookOpenIcon className="w-16 h-16 md:w-[5rem] md:h-[5rem] " />
               <h3 className="text-lg font-medium md:text-xl">Reading books</h3>
             </div>
 
-            <div className="flex items-center justify-center flex-col gap-2 dark:text-gray-200">
+            <div className="flex items-center justify-center flex-col gap-2 dark:text-gray-200 hover:text-orange-400 duration-300">
               <TvIcon className="w-16 h-16 md:w-[5rem] md:h-[5rem] " />
               <h3 className="text-lg font-medium md:text-xl">
                 Watching documentaries
               </h3>
             </div>
 
-            <div className="flex items-center justify-center flex-col gap-2 dark:text-gray-200">
+            <div className="flex items-center justify-center flex-col gap-2 dark:text-gray-200 hover:text-orange-400 duration-300">
               <CodeBracketIcon className="w-16 h-16 md:w-[5rem] md:h-[5rem] " />
               <h3 className="text-lg font-medium md:text-xl">Coding</h3>
             </div>
