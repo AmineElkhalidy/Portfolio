0bbc905abe39a0f2ce68e46d5df0991f63a35440
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2023-08-22 10:25:47 +0100

    Nav menuu has been fixed!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   return (
     <nav className="nav">
       <a
-        className="inline-flex dark:text-white items-center font-nav font-bold tracking-wide duration-300 text-black  sm:text-xl"
+        className="inline-flex dark:text-white items-center font-nav font-bold tracking-wide duration-300 text-black text-lg  sm:text-xl"
         href="#"
       >
         {/* <img className="h-[2rem]" src={Avatar} alt="Avatar" /> */}
@@ -25,70 +25,72 @@ const Navbar = () => {
 
       {/* Mobile navigation */}
       {open ? (
-        <motion.div
-          className={`fixed top-0 z-50 w-[80%] border-l-2 h-screen flex flex-col justify-center items-center bg-opacity-50 backdrop-blur-xl duration-[0.4s] drop-shadow-md md:hidden ${
+        <div
+          className={`fixed top-0 z-50 w-[80%] border-l-2 h-screen bg-opacity-50 backdrop-blur-xl duration-300 drop-shadow-md md:hidden ${
             open ? "right-0" : "right-[-100%]"
           } `}
         >
-          <ul className="nav__list">
-            <li>
-              <a
-                className="nav__link"
-                href="#about"
-                onClick={() => setOpen(false)}
-              >
-                About
-              </a>
-            </li>
+          <div className="h-full flex flex-col justify-center items-center">
+            <ul className="nav__list">
+              <li>
+                <a
+                  className="nav__link"
+                  href="#about"
+                  onClick={() => setOpen(false)}
+                >
+                  About
+                </a>
+              </li>
 
-            <li>
-              <a
-                className="nav__link"
-                href="#work"
-                onClick={() => setOpen(false)}
-              >
-                Work
-              </a>
-            </li>
+              <li>
+                <a
+                  className="nav__link"
+                  href="#work"
+                  onClick={() => setOpen(false)}
+                >
+                  Work
+                </a>
+              </li>
 
-            <li>
-              <a
-                className="nav__link"
-                href="#skills"
-                onClick={() => setOpen(false)}
-              >
-                Skills
-              </a>
-            </li>
+              <li>
+                <a
+                  className="nav__link"
+                  href="#skills"
+                  onClick={() => setOpen(false)}
+                >
+                  Skills
+                </a>
+              </li>
 
-            <li>
-              <a
-                className="nav__link"
-                href="#feedbacks"
-                onClick={() => setOpen(false)}
-              >
-                Testimonials
-              </a>
-            </li>
+              <li>
+                <a
+                  className="nav__link"
+                  href="#feedbacks"
+                  onClick={() => setOpen(false)}
+                >
+                  Testimonials
+                </a>
+              </li>
 
-            <li>
-              <a
-                className="nav__link"
-                href="#contact"
-                onClick={() => setOpen(false)}
-              >
-                Contact
-              </a>
-            </li>
-          </ul>
+              <li>
+                <a
+                  className="nav__link"
+                  href="#contact"
+                  onClick={() => setOpen(false)}
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
 
-          <div
-            className="inline-block absolute top-6 right-8 cursor-pointer duration-300"
-            onClick={() => setOpen(false)}
-          >
-            <XMarkIcon className="w-7 h-7 hover:text-grey-300 dark:text-gray-100 " />
+            <div
+              className="inline-block absolute top-6 right-8 cursor-pointer duration-300"
+              onClick={() => setOpen(false)}
+            >
+              <XMarkIcon className="w-7 h-7 hover:text-grey-300 dark:text-gray-100 " />
+            </div>
           </div>
-        </motion.div>
+        </div>
       ) : (
         <div className="flex items-center gap-3 duration-300 md:hidden">
           <Switcher />
