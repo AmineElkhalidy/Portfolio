f387935ab3c37fcd1440c31d0de7195c3043a518
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2023-09-11 18:34:05 +0100

    tailwind css animated removed!

--- src/components/atoms/Heading.jsx
+++ src/components/atoms/Heading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Heading = ({ children, className }) => {
   return (
     <h2
-      className={`text-black-900 dark:text-white text-2xl text-center font-nav font-bold mb-12 lg:mb-16 sm:text-3xl md:text-4xl lg:text-5xl animate-fade-down animate-duration-[2500ms] ${className}`}
+      className={`text-black-900 dark:text-white text-2xl text-center font-nav font-bold mb-12 lg:mb-16 sm:text-3xl md:text-4xl lg:text-5xl  ${className}`}
     >
       {children}
     </h2>
