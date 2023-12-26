31f610824169068a5399ac4fb9f467fd82234d91
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2023-12-26 17:39:25 +0100

    Loader has been added!

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,17 @@ import {
   Hobbies,
 } from "./sections";
 import { ArrowUpIcon } from "@heroicons/react/24/outline";
+import { RingLoader } from "react-spinners";
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  let [color, setColor] = useState("#17c969");
   const [showArrow, setShowArrow] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => setLoading(false), 2000);
+  }, []);
   useEffect(() => {
     const sections = document.querySelectorAll("section[id]");
 
@@ -55,24 +63,38 @@ const App = () => {
 
   return (
     <>
-      <Header />
-      <main className="overflow-hidden">
-        <Hero />
-        <About />
-        <Projects />
-        {/* <Skills /> */}
-        <Testimonials />
-        <Hobbies />
-        <Contact />
-      </main>
+      {loading ? (
+        <div className="h-screen w-full flex justify-center items-center">
+          <RingLoader
+            color={color}
+            loading={loading}
+            size={100}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <>
+          <Header />
+          <main className="overflow-hidden">
+            <Hero />
+            <About />
+            <Projects />
+            {/* <Skills /> */}
+            <Testimonials />
+            <Hobbies />
+            <Contact />
+          </main>
 
-      <a
-        href="#"
-        className={`scrollup ${!showArrow ? "-right-20" : "right-2"}`}
-        aria-labelledby="Go Up"
-      >
-        <ArrowUpIcon className="scrollup-icon" />
-      </a>
+          <a
+            href="#"
+            className={`scrollup ${!showArrow ? "-right-20" : "right-2"}`}
+            aria-labelledby="Go Up"
+          >
+            <ArrowUpIcon className="scrollup-icon" />
+          </a>
+        </>
+      )}
     </>
   );
 };
