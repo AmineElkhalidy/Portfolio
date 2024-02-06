9e6f61e6ec4e2b9f1954b31e34cc5b7c3bd254b3
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2024-02-06 21:02:57 +0100

    Added background to the hero section!

--- src/components/atoms/MainButton.jsx
+++ src/components/atoms/MainButton.jsx
@@ -4,13 +4,13 @@ const MainButton = ({ children }) => {
   return (
     <div className="relative">
       <a
-        className="inline-block px-[31px] py-5 bg-black-900 dark:text-black-900 dark:bg-gray-100 text-white cursor-pointer text-lg duration-300 font-nav font-semibold z-10 hover:translate-x-2 hover:translate-y-2"
+        className="inline-block z-50 px-[31px] py-5 bg-black-900 text-white cursor-pointer text-lg duration-300 font-nav font-semibold hover:translate-x-2 hover:translate-y-2"
         href="#contact"
       >
         {children}
       </a>
 
-      <span className="contact-btn absolute -z-10 top-2 left-2 px-[4.95rem] py-[2.15rem]" />
+      <div className="contact-btn -z-10 absolute top-2 left-2 px-[4.95rem] py-[2.15rem]" />
     </div>
   );
 };
