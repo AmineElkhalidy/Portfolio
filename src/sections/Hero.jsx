bc08515cdf64a81dcb533ab03fc4ac4f08bd6f48
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2024-02-07 13:24:51 +0100

    Returns to old shape!

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,24 +1,15 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import TypeIt from "typeit-react";
 import { ArrowDownTrayIcon } from "@heroicons/react/24/solid";
 import CV from "../assets/pdf/Amine-Elkhalidy.pdf";
 import ParentContainer from "../components/Container";
 import MainButton from "../components/atoms/MainButton";
-import { Gradient } from "../tools/gradient";
 
 const Hero = () => {
-  // Create your instance
-  const gradient = new Gradient();
-
-  useEffect(() => {
-    // Call `initGradient` with the selector to your canvas
-    gradient.initGradient("#gradient-canvas");
-  }, []);
-
   return (
     <section className="relative pb-10">
-      <div className="p-container z-50">
+      <ParentContainer>
         <div className="relative">
           <motion.div
             initial={{ opacity: 0 }}
@@ -58,7 +49,7 @@ const Hero = () => {
                       viewBox="0 0 50 50"
                       width="50px"
                       height="50px"
-                      className="w-7 h-full  fill-black-900"
+                      className="w-7 h-full dark:fill-white fill-black-900"
                     >
                       <path d="M 1 9 C 0.448 9 0 9.448 0 10 L 0 24.115234 C 0 30.276234 4.6829844 35.631469 10.833984 35.980469 C 16.730984 36.314469 21.747875 32.176391 22.796875 26.650391 C 23.366875 27.420391 23.94625 28.135969 24.53125 28.792969 L 21.001953 43.771484 C 20.932953 44.069484 21.002406 44.380094 21.191406 44.621094 C 21.382406 44.859094 21.670563 45 21.976562 45 L 27.095703 45 C 27.559703 45 27.959406 44.687328 28.066406 44.236328 C 28.687406 41.615328 29.660969 37.508156 30.542969 33.785156 L 31.453125 34.3125 C 33.618125 35.4335 35.815 36 38 36 C 45.192 36 50.926 29.641 49.875 22.25 C 49.179 17.354 45.414078 13.318344 40.580078 12.277344 C 34.951078 11.064344 29.525359 13.949141 27.193359 18.744141 C 27.193359 18.744141 26.934562 19.318578 26.726562 19.892578 C 25.076563 17.046578 24.107609 14.094078 23.599609 11.955078 C 23.414609 11.175078 23.137469 9.762875 23.105469 9.671875 C 22.966469 9.268875 22.589109 9 22.162109 9 L 17 9 C 16.448 9 16 9.448 16 10 L 16 24.300781 C 16 26.649781 14.287219 28.750516 11.949219 28.978516 C 9.2672187 29.240516 7 27.13 7 24.5 L 7 10 C 7 9.448 6.552 9 6 9 L 1 9 z M 38 19 C 40.757 19 43 21.243 43 24 C 43 26.757 40.757 29 38 29 C 35.775 29 33.713531 27.658281 32.269531 26.488281 C 32.796531 24.263281 33.15025 22.773047 33.15625 22.748047 C 33.72425 20.541047 35.717 19 38 19 z" />
                     </svg>
@@ -70,7 +61,7 @@ const Hero = () => {
                     aria-label="Fiverr Profile"
                   >
                     <svg
-                      className="w-[1.6rem]  fill-black-900"
+                      className="w-[1.6rem] dark:fill-white fill-black-900"
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 16 16"
                     >
@@ -87,7 +78,7 @@ const Hero = () => {
                     aria-label="Github Profile"
                   >
                     <svg
-                      className="fill-black-900 w-[1.85rem] -ml-[.20rem] h-full"
+                      className="fill-black-900 dark:fill-white w-[1.85rem] -ml-[.20rem] h-full"
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 24 24"
                     >
@@ -108,7 +99,7 @@ const Hero = () => {
                       viewBox="0 0 50 50"
                       width="24px"
                       height="24px"
-                      className="w-7 fill-black-900 "
+                      className="w-7 fill-black-900 dark:fill-white"
                     >
                       <path d="M 8 3.0097656 C 4.53 3.0097656 2.0097656 5.0892187 2.0097656 7.9492188 C 2.0097656 10.819219 4.59 12.990234 8 12.990234 C 11.47 12.990234 13.990234 10.870625 13.990234 7.890625 C 13.830234 5.020625 11.36 3.0097656 8 3.0097656 z M 3 15 C 2.45 15 2 15.45 2 16 L 2 45 C 2 45.55 2.45 46 3 46 L 13 46 C 13.55 46 14 45.55 14 45 L 14 16 C 14 15.45 13.55 15 13 15 L 3 15 z M 18 15 C 17.45 15 17 15.45 17 16 L 17 45 C 17 45.55 17.45 46 18 46 L 27 46 C 27.552 46 28 45.552 28 45 L 28 30 L 28 29.75 L 28 29.5 C 28 27.13 29.820625 25.199531 32.140625 25.019531 C 32.260625 24.999531 32.38 25 32.5 25 C 32.62 25 32.739375 24.999531 32.859375 25.019531 C 35.179375 25.199531 37 27.13 37 29.5 L 37 45 C 37 45.552 37.448 46 38 46 L 47 46 C 47.55 46 48 45.55 48 45 L 48 28 C 48 21.53 44.529063 15 36.789062 15 C 33.269062 15 30.61 16.360234 29 17.490234 L 29 16 C 29 15.45 28.55 15 28 15 L 18 15 z" />
                     </svg>
@@ -216,13 +207,7 @@ const Hero = () => {
             </div>
           </motion.div>
         </div>
-      </div>
-
-      <canvas
-        className="absolute top-0 left-0 -z-50 opacity-40"
-        id="gradient-canvas"
-        data-transition-in
-      />
+      </ParentContainer>
     </section>
   );
 };
