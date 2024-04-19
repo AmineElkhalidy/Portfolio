ac4afd9e67cab909cb1cc9c01c334678fe9b9696
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2024-04-19 10:59:17 +0100

    Roundness fixed!

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,60 +1,67 @@
-import { ArrowRightIcon } from "@heroicons/react/24/solid";
-import Link from "./atoms/Link";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { urlFor } from "@/client";
 
-const Project = ({ project }: { project: any }) => {
+const Project = ({ work }: { work: any }) => {
   return (
-    <div className="project__container">
-      <div className="relative">
-        <div className="h-[250px] sm:h-[350px]">
-          <img
-            className="h-full border-2 border-black-900 object-cover cursor-pointer"
-            src={urlFor(project.imgUrl).url() || ""}
-            alt={project.title}
-          />
-        </div>
-
-        <div className="w-full bg-[#111827] p-5 sm:p-8 lg:p-12 -mt-2">
-          <h2 className="text-white text-xl font-bold mb-4 lg:text-2xl lg:mb-6 2xl:text-3xl">
-            {project.title}
-          </h2>
-
-          <div className="w-full flex gap-4 mb-12">
-            {project?.techs?.length > 0 &&
-              project.techs.map((tech: any, index: any) => (
-                <div className="w-[11%] h-[11%]" key={index}>
-                  <img
-                    className="w-full h-full rounded-full"
-                    src={urlFor(tech).url()}
-                    alt="Web tech"
-                  />
+    <Sheet>
+      <SheetTrigger asChild>
+        <div className="bg-white cursor-pointer">
+          <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl">
+            <div className="relative border-2 border-[#111827] overflow-hidden rounded-lg lg:h-[450px]">
+              <div className="absolute inset-0">
+                <img
+                  src={urlFor(work?.imgUrl).url()}
+                  alt=""
+                  className="h-full w-full object-cover"
+                />
+              </div>
+              <div
+                aria-hidden="true"
+                className="relative h-96 w-full lg:hidden"
+              />
+              <div
+                aria-hidden="true"
+                className="relative h-32 w-full lg:hidden"
+              />
+              <div className="absolute inset-x-0 bottom-0 bg-[#111827] bg-opacity-80 p-6 backdrop-blur backdrop-filter sm:flex sm:items-center sm:justify-between lg:inset-x-auto lg:inset-y-0 lg:w-96 lg:flex-col lg:items-start">
+                <div>
+                  <h2 className="text-xl font-bold text-white">
+                    {work?.title}
+                  </h2>
+                  <p className="mt-1 text-sm text-gray-300">
+                    {work?.description}
+                  </p>
                 </div>
-              ))}
-          </div>
-
-          <div className="flex justify-between mt-6 lg:text-lg">
-            <Link
-              href={project.projectLink}
-              target="_blank"
-              className="p-3 sm:px-5 sm:py-3 gradient group inline-flex items-center sm:font-medium text-md gap-1 lg:text-lg cursor-pointer text-white font-semibold"
-            >
-              <span>View Project</span>
-
-              <ArrowRightIcon className="w-4 h-4 duration-300 group-hover:translate-x-1 text-white " />
-            </Link>
-
-            <Link
-              href={project.codeLink}
-              target="_blank"
-              className="group inline-flex items-center sm:font-medium text-md gap-1 lg:text-lg text-white cursor-pointer"
-            >
-              <span>Github Repo</span>
-              <ArrowRightIcon className="w-4 h-4 duration-300 group-hover:translate-x-1" />
-            </Link>
+                <a
+                  href={work?.projectLink}
+                  target="_blank"
+                  className="mt-6 flex flex-shrink-0 items-center justify-center rounded-md border border-white border-opacity-25 bg-white bg-opacity-0 px-4 py-3 text-base font-medium text-white hover:bg-opacity-10 sm:ml-8 sm:mt-0 lg:ml-0 lg:w-full"
+                >
+                  View project
+                </a>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </SheetTrigger>
+
+      <SheetContent className="">
+        <SheetHeader>
+          <SheetTitle>{work?.title}</SheetTitle>
+          <SheetDescription>{work?.description}</SheetDescription>
+        </SheetHeader>
+        <div></div>
+        <SheetFooter></SheetFooter>
+      </SheetContent>
+    </Sheet>
   );
 };
 
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,9 +1,11 @@
+//@ts-nocheck
 import { useState, useEffect } from "react";
 import ParentContainer from "../components/Container";
 import { client, urlFor } from "@/client";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import { ArrowRightIcon, ArrowLeftIcon } from "@heroicons/react/24/solid";
+import Project from "@/components/Project";
 
 const Projects = () => {
   const [works, setWorks] = useState([]);
@@ -50,44 +52,7 @@ const Projects = () => {
             )}
           >
             {works.map((work, index) => (
-              <div key={index} className="bg-white">
-                <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl">
-                  <div className="relative border-2 border-[#111827] overflow-hidden rounded-lg lg:h-[450px]">
-                    <div className="absolute inset-0">
-                      <img
-                        src={urlFor(work?.imgUrl).url()}
-                        alt=""
-                        className="h-full w-full object-cover"
-                      />
-                    </div>
-                    <div
-                      aria-hidden="true"
-                      className="relative h-96 w-full lg:hidden"
-                    />
-                    <div
-                      aria-hidden="true"
-                      className="relative h-32 w-full lg:hidden"
-                    />
-                    <div className="absolute inset-x-0 bottom-0 rounded-bl-lg rounded-br-lg bg-[#111827] bg-opacity-80 p-6 backdrop-blur backdrop-filter sm:flex sm:items-center sm:justify-between lg:inset-x-auto lg:inset-y-0 lg:w-96 lg:flex-col lg:items-start lg:rounded-br-none lg:rounded-tl-lg">
-                      <div>
-                        <h2 className="text-xl font-bold text-white">
-                          {work?.title}
-                        </h2>
-                        <p className="mt-1 text-sm text-gray-300">
-                          {work?.description}
-                        </p>
-                      </div>
-                      <a
-                        href={work?.projectLink}
-                        target="_blank"
-                        className="mt-6 flex flex-shrink-0 items-center justify-center rounded-md border border-white border-opacity-25 bg-white bg-opacity-0 px-4 py-3 text-base font-medium text-white hover:bg-opacity-10 sm:ml-8 sm:mt-0 lg:ml-0 lg:w-full"
-                      >
-                        View project
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <Project work={work} key={index} />
             ))}
           </Carousel>
         </div>
