6108d0bf2c21c5a4fa365cbf0018c7f8f15d658f
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2024-05-26 19:01:40 +0100

    47- New change added!

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -198,7 +198,7 @@ const Hero = () => {
                   stroke-width="2"
                   stroke-linecap="round"
                   stroke-linejoin="round"
-                  class="lucide lucide-instagram fill-gray-200 duration-300 hover:scale-110"
+                  className="lucide lucide-instagram fill-gray-200 duration-300 hover:scale-110"
                 >
                   <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
                   <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
